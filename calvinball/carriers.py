8f447337ffea4dff6d77dd9e4b1d977c756b35dd
Jason Jones <jasonedwardjones@gmail.com>
2023-09-28 13:02:17 -0800

    Fix TACAN for A4

--- calvinball/carriers.py
+++ calvinball/carriers.py
@@ -10,6 +10,9 @@ class Carriers:
         country = m.country(dcs.countries.CombinedJointTaskForcesBlue.name)
         for carrier_name, carrier in blue_carriers.items():
             sg = m.ship_group(country, carrier_name, carrier["ship"], dcs.mapping.Point(carrier["position"]["x"], carrier["position"]["y"], m.terrain), heading=90)
+            sg.points[0].tasks.append(dcs.task.ActivateBeaconCommand(carrier["tacan_channel"], "X", carrier_name[:3], True, sg.units[0].id,  aa=False))
+            if "icls_channel" in carrier:
+                sg.points[0].tasks.append(dcs.task.ActivateICLSCommand(carrier["icls_channel"], sg.units[0].id))
             if edit:
                 if "waypoints" in carrier:
                     for wp in carrier["waypoints"]:
