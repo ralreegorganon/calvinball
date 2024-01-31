496ce59ed4796483316daa914b402c1ddac9384c
Jason Jones <jasonedwardjones@gmail.com>
2024-01-31 09:43:17 -0900

    Disable markerops in cyprus

---
+++ missions/CyprusInvasion/resources/config.lua
@@ -0,0 +1,5 @@
+MissionDb.markerops.enableBomb = false
+MissionDb.markerops.enableStrike = false
+MissionDb.markerops.enableSead = false
+MissionDb.markerops.enableCas = false
+MissionDb.markerops.enableCarpetbomb = false
